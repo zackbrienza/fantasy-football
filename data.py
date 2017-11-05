8f4097e50651566ca66b42241fa84c4bc346a1b4
Zack Brienza <zb43@cornell.edu>
2017-11-05 03:21:07 -0500

    Finished RB data collection function

--- data.py
+++ data.py
@@ -1,7 +1,20 @@
 import nflgame
 
-games = nflgame.games(2013, week=1)
-players = nflgame.combine_game_stats(games)
-for p in players.rushing().sort('rushing_yds').limit(5):
-    msg = '%s %d carries for %d yards and %d TDs'
-    print (msg % (p, p.rushing_att, p.rushing_yds, p.rushing_tds))
+def rb_weekly_stats (year):
+  d={}
+  for week in range (17):
+    games = nflgame.games(int(year), int(week+1))
+    players = nflgame.combine_game_stats(games)
+    for p in players.rushing().sort('rushing_yds'):
+      if str(p) in d:
+        d[str(p)].append ((p.rushing_att, p.rushing_yds, p.rushing_tds))
+      else:
+        d[str(p)] = [((p.rushing_att, p.rushing_yds, p.rushing_tds))]
+  return d
+        #msg = '%s %d carries for %d yards and %d TDs'
+        #print (msg % (p, p.rushing_att, p.rushing_yds, p.rushing_tds))
+
+
+
+if __name__ == '__main__':
+  rb_weekly_stats ((raw_input("year: ")))
