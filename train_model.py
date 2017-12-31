76817644c2ce2ec7df18567398d3c035edb28bea
Scott Dickson <sdd48@cornell.edu>
2017-12-31 00:11:47 -0500

    Support for quadratic features

--- train_model.py
+++ train_model.py
@@ -1,6 +1,6 @@
 #Scott Dickson
 #12/21/2017
-#Framework to train the linear prediction model
+#Framework to train various prediction models
 #dim(X):  n by d
 #dim(Y): n by 1
 #dim(Theta): d by 1
@@ -9,26 +9,55 @@
 import numpy as np
 
 #Given data and parameters return the current prediction error
-def error(X, Y, Theta):
+def linear_error(X, Y, Theta):
     A = np.dot(X,Theta) - Y
     return np.dot(A,A)
-    
+#Here theta must be of size 2d
+def quadratic_error(X,Y,Theta):
+    d = len(X[0])
+    A = np.dot(X,Theta[0:d]) + np.dot(X**2,Theta[d:]) - Y
+    return np.dot(A.T,A)
+
+   
 #Given data and current parameters return the gradient vector of the error function
-def gradient(X, Y, Theta):
-    return 2*np.dot(X.T,(np.dot(X,Theta) - Y))
+def linear_gradient(X, Y, Theta):
+    return 2*np.dot(X.T,np.dot(X,Theta) - Y)
+
+#Gradient of a model in the form of f(X,Theta) = x * Theta + x^2 * Theta
+def quadratic_gradient(X, Y, Theta):
+    d = len(X[0])
+    X2 = X**2
+        
+    p1 = 2*np.dot(X.T, np.dot(X,Theta[0:d]) + np.dot(X2,Theta[d:]) - Y)
+    p2 = 2*np.dot(X2.T, np.dot(X,Theta[0:d]) + np.dot(X2,Theta[d:]) - Y)
+  
+    return np.concatenate([p1,p2])
     
 #Run gradient descent for up to t steps
-def optimize(x, y, alpha=0.05, t = 1000):
+def optimize(x, y, alpha=0.05, t=1000):
     d = len(x[0])
     param = np.zeros((d,1)) #Initialize the parameter vector
     steps = 0
     while(steps < t):
-        param -= alpha*gradient(x,y,param)
+        param -= alpha*linear_gradient(x,y,param)
         steps += 1
     return param
+
+#Also run gradient descent for up to t steps
+def quad_optimize(x,y,alpha=0.005, t=1000):
+    d = len(x[0])
+    param = np.zeros((2*d,1))
+    steps = 0
+    
+    while(steps < t):
+        param -= alpha*quadratic_gradient(x,y,param)
+        steps += 1
+    print("Training Error: " + str(quadratic_error(x,y,param)[0][0]))    
+    return param
+
 
 if __name__ == '__main__':
     theta = np.array([[1],[2],[3]])      
     x = np.array([[1,1,1],[0,0,2]])
     y = np.array([[6], [6]])
-    print (optimize(x,y))
+    print (quad_optimize(x,y))
