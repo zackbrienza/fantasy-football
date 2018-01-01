5cc73473229f684e1fb9ecd1d38989866b4f915b
Scott Dickson <sdd48@cornell.edu>
2018-01-01 17:45:52 -0500

    Finished and fixed polynomial model

--- train_model.py
+++ train_model.py
@@ -23,8 +23,8 @@ def quadratic_error(X,Y,Theta):
 def polynomial_error(X,Y, Theta):
     d = len(X[0])
     n = len(Theta) / d
-    A =  (-1)*Y #Initialize sum
-   
+    A = (-1.0)*Y #Initialize sum
+    #Fix the above
     for i in range(n):
         A += np.dot(X**(i+1),Theta[i*d:(i+1)*d])
     return A
@@ -47,18 +47,21 @@ def quadratic_gradient(X, Y, Theta):
 def polynomial_gradient(X,Y,Theta):
     d = len(X[0])
     n = len(Theta) / d
-    grad = np.array([])
+    grad = np.zeros((0,1))
     
     #Figure out the base part of the gradient
-    base_grad = (-1)*Y
+    base_grad = (-1.0)*Y
     
     for i in range(n):
         base_grad += np.dot(X**(i+1), Theta[d*i:d*(i+1)])
     
+    
     #Figure out each section of the gradient vector    
     for j in range(n):
-        grad.concatenate(grad,2*np.dot(X**(i+1).T, base_grad))
-    
+        #print(grad.shape)
+        #print(np.dot((X**(i+1)).T, base_grad).shape) #Get a factor of 2 in here
+        grad = np.concatenate([grad,2*np.dot((X**(j+1)).T, base_grad)])
+    #print(grad.shape)    
     return grad 
        
 #Run gradient descent for up to t steps
@@ -94,10 +97,10 @@ def polynomial_optimize(x,y,deg=2,alpha=0.005, t=1000):
         param -= alpha*polynomial_gradient(x,y,param)
         steps += 1
     print("Training Error: " + str(polynomial_error(x,y,param)))
-    return pararm 
+    return param 
 
 if __name__ == '__main__':
     theta = np.array([[1],[2],[3]])      
     x = np.array([[1,1,1],[0,0,2]])
     y = np.array([[6], [6]])
-    print (quad_optimize(x,y))
+    print (polynomial_optimize(x,y,2))
