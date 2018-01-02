f81d8118c05a96d6cfaadd994a9473da642f8f9b
Scott Dickson <sdd48@cornell.edu>
2018-01-01 23:55:19 -0500

    Added option for regularization and stub for deep learning approach

--- train_model.py
+++ train_model.py
@@ -10,7 +10,7 @@ import numpy as np
 
 #Given data and parameters return the current prediction error
 def linear_error(X, Y, Theta):
-    A = np.dot(X,Theta) - Y
+    A = np.dot(X,Theta) - Y #+ np.dot(Theta, Theta)
     return np.dot(A,A)
 
 #Here theta must be of size 2d
@@ -30,21 +30,21 @@ def polynomial_error(X,Y, Theta):
     return A
    
 #Given data and current parameters return the gradient vector of the error function
-def linear_gradient(X, Y, Theta):
-    return 2*np.dot(X.T,np.dot(X,Theta) - Y)
+def linear_gradient(X, Y, Theta, l=0):
+    return 2*np.dot(X.T,np.dot(X,Theta) - Y) + l*Theta
 
 #Gradient of a model in the form of f(X,Theta) = x * Theta + x^2 * Theta
-def quadratic_gradient(X, Y, Theta):
+def quadratic_gradient(X, Y, Theta,l=0):
     d = len(X[0])
     X2 = X**2
         
     p1 = 2*np.dot(X.T, np.dot(X,Theta[0:d]) + np.dot(X2,Theta[d:]) - Y)
     p2 = 2*np.dot(X2.T, np.dot(X,Theta[0:d]) + np.dot(X2,Theta[d:]) - Y)
   
-    return np.concatenate([p1,p2])
+    return np.concatenate([p1,p2]) + l*Theta
  
 #Same as above but for general polynomial functions
-def polynomial_gradient(X,Y,Theta):
+def polynomial_gradient(X,Y,Theta,l=0):
     d = len(X[0])
     n = len(Theta) / d
     grad = np.zeros((0,1))
@@ -62,39 +62,39 @@ def polynomial_gradient(X,Y,Theta):
         #print(np.dot((X**(i+1)).T, base_grad).shape) #Get a factor of 2 in here
         grad = np.concatenate([grad,2*np.dot((X**(j+1)).T, base_grad)])
     #print(grad.shape)    
-    return grad 
+    return grad + l*Theta
        
 #Run gradient descent for up to t steps
-def optimize(x, y, alpha=0.05, t=1000):
+def optimize(x, y, alpha=0.05, t=1000,l=0):
     d = len(x[0])
     param = np.zeros((d,1)) #Initialize the parameter vector
     steps = 0
     while(steps < t):
-        param -= alpha*linear_gradient(x,y,param)
+        param -= alpha*linear_gradient(x,y,param,l)
         steps += 1
     return param
 
 #Also run gradient descent for up to t steps
-def quad_optimize(x,y,alpha=0.005, t=1000):
+def quad_optimize(x,y,alpha=0.005, t=1000,l=0):
     d = len(x[0])
     param = np.zeros((2*d,1))
     steps = 0
     
     while(steps < t):
-        param -= alpha*quadratic_gradient(x,y,param)
+        param -= alpha*quadratic_gradient(x,y,param,l)
         steps += 1
     print("Training Error: " + str(quadratic_error(x,y,param)[0][0]))    
     return param
 
 #Again, gradient descent for up to t steps but this time fit
 #a polynomial model of maximum degree [deg]
-def polynomial_optimize(x,y,deg=2,alpha=0.005, t=1000):
+def polynomial_optimize(x,y,deg=2,alpha=0.0005, t=1000,l=0):
     d = len(x[0])
     param = np.zeros((deg*d,1))
     steps = 0
     
     while(steps < t):
-        param -= alpha*polynomial_gradient(x,y,param)
+        param -= alpha*polynomial_gradient(x,y,param,l)
         steps += 1
     print("Training Error: " + str(polynomial_error(x,y,param)))
     return param 
@@ -103,4 +103,4 @@ if __name__ == '__main__':
     theta = np.array([[1],[2],[3]])      
     x = np.array([[1,1,1],[0,0,2]])
     y = np.array([[6], [6]])
-    print (polynomial_optimize(x,y,2))
+    print (quad_optimize(x,y,0.005,1000,0))
