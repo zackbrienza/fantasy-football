c03fe15b0011ac2b0cc453d583fbbcec7ff47fc4
Scott Dickson <sdd48@cornell.edu>
2018-01-03 20:03:56 -0500

    Max flow fixed and working

--- graphs.py
+++ graphs.py
@@ -21,7 +21,6 @@ def bfs(adj, start, end):
         
     while not q.empty():
         temp = q.get()
-        print(temp)
         visited[temp] = True
         
         if temp == end:
@@ -33,17 +32,19 @@ def bfs(adj, start, end):
     
     #Path not found
     if not visited[end]:
-        print(visited)
         return []
     else:
     #Backtrack and recover the path    
         cur = end
+        bt_visited = n*[False]
         
         while cur != start:
+            bt_visited[cur] = True
             for i in range(n):
-                if adj[i][cur] == 1 and visited[i]:
+                if adj[i][cur] == 1 and visited[i] and not bt_visited[i]:
                     path = [[i,cur]] + path
                     cur = i
+                    break
         return path
                            
 
@@ -51,7 +52,7 @@ def bfs(adj, start, end):
 #Generate a maximum flow assignment of flow on each edge
 def ford_fulkerson(adj,cap,s,t):
     n = len(adj[0])
-    flow = [[0 for x in range(num)] for y in range(num)] #No flow on the graph initially
+    flow = [[0 for x in range(n)] for y in range(n)] #No flow on the graph initially
     max_flow = False
     
     while not max_flow:
@@ -71,15 +72,15 @@ def ford_fulkerson(adj,cap,s,t):
 #Adjacency list 
 def makeResidual(adj,flow,cap):
     n = len(adj[0])
-    res = adj
+    res = [[0 for x in range(n)] for y in range(n)]
     
     for i in range(n):
         for j in range(n):
             if adj[i][j] == 1:
-                if flow[i][j] == cap[i][j]:
-                    res[i][j] = 0
                 if flow[i][j] > 0:
                     res[j][i] = 1 #add backwards edge
+                if flow[i][j] < cap[i][j]:
+                    res[i][j] = 1 #Add forward edge
     return res
 
 #Update the flow after an augmenting path has been found
@@ -100,9 +101,7 @@ def augment(adj,flow,cap,path):
         elif adj[tail][head] == 1: #Check for backwards edges    
             diff = flow[head][tail]
             
-            if min_diff == 0:
-                min_diff = diff
-            elif min_diff > diff:
+            if min_diff == 0 or min_diff > diff:
                 min_diff = diff
                 
     #Now add/subtract [min_diff] amount of flow
@@ -113,16 +112,12 @@ def augment(adj,flow,cap,path):
         tail = path[i][1]
         
         if adj[head][tail] == 1:
-            flow[head][tail] += diff
+            flow[head][tail] += min_diff
         elif adj[tail][head] == 1:
-            flow[head][tail] -= diff
+            flow[head][tail] -= min_diff
+    
     return flow
         
-
-
-
-    
-
 
 #Graph class for easy use and testing 
 class Graph:
@@ -130,36 +125,48 @@ class Graph:
     def __init__(self):
         self.n = 0
         self.adj = []
-
+        self.cap = []
+        
     def setNodes(self,num):
         self.n = num
         self.adj = [[0 for x in range(num)] for y in range(num)]
-    def add_edge(self,start,end):
+        self.cap = [[0 for x in range(num)] for y in range(num)]
+    def add_edge(self,start,end,c=1):
         self.adj[start][end] = 1
+        self.cap[start][end] = c #Default capacity
     def getAdj(self):
         return self.adj
-    
-
-
-
-
+    def getCap(self):
+        return self.cap
 
 if __name__ == '__main__':
     g = Graph()
     
-    g.setNodes(8)
-    g.add_edge(0,1)
-    g.add_edge(0,5)
-    g.add_edge(1,5)
-    g.add_edge(5,6)
-    g.add_edge(6,7)
-    g.add_edge(5,7)
+    g.setNodes(7)
+    g.add_edge(0,1,3)
+    g.add_edge(0,2,5)
+
     
-    g.add_edge(1,2)
-    g.add_edge(2,3)
-    g.add_edge(3,4)
+    g.add_edge(1,3,1)
+    g.add_edge(2,3,2)
     
-    print(bfs(g.getAdj(),0,4))
+    g.add_edge(1,4,5)
+    g.add_edge(3,4,1)
+    
+    g.add_edge(3,5,3)
+    g.add_edge(2,5,2)
+    
+    g.add_edge(4,6,7)
+    g.add_edge(5,6,3)
+    
+    print("Adjacency array:")
+    print(g.getAdj())
+    print("Capacity array:")
+    print(g.getCap())
+    
+    #Test with all edges having cap 1
+    print("Max flow array:")
+    print(ford_fulkerson(g.getAdj(),g.getCap(),0,6))
     
     
     
